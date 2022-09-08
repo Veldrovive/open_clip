be7b72ccd8ef2d683f9102a26e243f503d4b9447
Aidan <aidan.dempster@gmail.com>
2022-09-08 23:07:12 +0000

    Improved 3d conv with a transformer
    Among other things

--- src/open_clip/model.py
+++ src/open_clip/model.py
@@ -453,6 +453,149 @@ class MLP(nn.Module):
     def forward(self, x: torch.Tensor):
         return self.layers(x)
 
+class ClassVoxel3dConvEncoder(nn.Module):
+    def __init__(self, dims: List[int], width: int, output_dim: int, act_layer: Callable = nn.GELU):
+        super().__init__()
+        self.channels = [64, 128, 256, 256, 256, 256, width]
+        self.strides = [1, 1, 1, 2, 1, 2, 2]
+        self.padding = [1, 1, 1, 1, 1, 1, 1]
+        self.dialation = [1, 1, 1, 1, 1, 1, 1]
+        self.kernel = [3, 3, 3, 3, 3, 3, 3]
+        assert len(self.channels) == len(self.strides) == len(self.padding) == len(self.dialation) == len(self.kernel), f"Lengths of channels, strides, padding, dialation, and kernel must be the same. Got {len(self.channels)}, {len(self.strides)}, {len(self.padding)}, {len(self.dialation)}, {len(self.kernel)}"
+        channels = [1] + self.channels
+        self.conv_blocks = nn.ModuleList([
+            self._get_conv_layer(channels[i], channels[i + 1], kernel_size=self.kernel[i], stride=self.strides[i], padding=self.padding[i], dilation=self.dialation[i], act_layer=act_layer)
+            for i in range(len(self.channels))
+        ])
+        for n in range(len(self.channels)):
+            stride = self.strides[n]
+            dialation = self.dialation[n]
+            padding = self.padding[n]
+            kernel = self.kernel[n]
+            dims = [int((d + 2*padding - dialation*(kernel - 1) - 1)/(stride) + 1) for d in dims]
+            logging.info(f"Conv {n} output shape: {dims}")
+        logging.info(f"Transformer sequence length: {np.prod(dims)} + 1. Transformer width: {width}")
+        self.class_embedding = nn.Parameter(width**-0.5 * torch.randn(width))
+        self.ln_pre = LayerNorm(width)
+        self.transformer = Transformer(width, layers=4, heads=16, mlp_ratio=4, act_layer=act_layer, dropout=0.0)
+        self.ln_post = LayerNorm(width)
+        self.proj = nn.Parameter(width**-0.5 * torch.randn(width, output_dim))
+
+    def _get_conv_layer(self, c_in, c_out, kernel_size, stride, padding, dilation, act_layer):
+        return nn.Sequential(
+            nn.Conv3d(c_in, c_out, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation),
+            nn.Dropout3d(0.1),
+            act_layer(),
+            # nn.MaxPool3d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x: torch.Tensor):
+        for block in self.conv_blocks:
+            x = block(x)
+        # Current shape: [*, output_dim, x, y, z]
+        x = x.reshape(x.shape[0], x.shape[1], -1) # [*, width, seq_len]
+        x = x.permute(0, 2, 1) # [*, seq_len, width]
+        # Add the class embedding
+        x = torch.concat([
+            self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device),
+            x
+        ], dim=1) # [*, 1 + seq_len, width]
+        x = self.ln_pre(x)
+        x = x.permute(1, 0, 2) # [1 + seq_len, *, width]
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2) # [*, 1 + seq_len, width]
+        x = self.ln_post(x[:, 0, :]) # [*, width]
+        x = x @ self.proj # [*, output_dim]
+        return x
+
+class NewVoxel3dConvEncoder(nn.Module):
+    def __init__(self, dims: List[int], attention_width: int, output_dim: int, average_output: bool = False, act_layer: Callable = nn.GELU):
+        super().__init__()
+        self.average_output = average_output  # Average the output of the transformer instead of using a flattened linear layer
+        self.channels = [64, 128, 256, 256, 256, attention_width]
+        self.strides = [1, 1, 1, 2, 2, 2]
+        self.padding = [1, 1, 1, 0, 1, 0]
+        self.dialation = [1, 1, 1, 1, 1, 1]
+        self.kernel = [3, 3, 3, 3, 3, 3]
+        assert len(self.channels) == len(self.strides) == len(self.padding) == len(self.dialation) == len(self.kernel), f"Lengths of channels, strides, padding, dialation, and kernel must be the same. Got {len(self.channels)}, {len(self.strides)}, {len(self.padding)}, {len(self.dialation)}, {len(self.kernel)}"
+        channels = [1] + self.channels
+        self.conv_blocks = nn.ModuleList([
+            self._get_conv_layer(channels[i], channels[i + 1], kernel_size=self.kernel[i], stride=self.strides[i], padding=self.padding[i], dilation=self.dialation[i], act_layer=act_layer)
+            for i in range(len(self.channels))
+        ])
+        for n in range(len(self.channels)):
+            stride = self.strides[n]
+            dialation = self.dialation[n]
+            padding = self.padding[n]
+            kernel = self.kernel[n]
+            dims = [int((d + 2*padding - dialation*(kernel - 1) - 1)/(stride) + 1) for d in dims]
+            logging.info(f"Conv {n} output shape: {dims}")
+        logging.info(f"Transformer sequence length: {np.prod(dims)}. Transformer width: {attention_width}")
+        self.transformer = Transformer(attention_width, layers=2, heads=8, mlp_ratio=4, act_layer=act_layer, dropout=0.0)
+        logging.info(f"Projection input features: {attention_width * dims[0] * dims[1] * dims[2]}")
+        if self.average_output:
+            self.proj = nn.Parameter(attention_width**-0.5 * torch.randn(attention_width, output_dim))
+        else:
+            self.proj = nn.Linear(attention_width * dims[0] * dims[1] * dims[2], output_dim)
+
+    def _get_conv_layer(self, c_in, c_out, kernel_size, stride, padding, dilation, act_layer):
+        return nn.Sequential(
+            nn.Conv3d(c_in, c_out, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation),
+            nn.Dropout3d(0.1),
+            act_layer(),
+            # nn.MaxPool3d(kernel_size=2, stride=2)
+        )
+
+    def forward(self, x: torch.Tensor):
+        for block in self.conv_blocks:
+            x = block(x)
+        # Currently the output shape is [*, attention_width, x, y, z]
+        x = x.reshape(x.shape[0], x.shape[1], -1) # [*, attention_width, seq_len]
+        x = x.permute(2, 0, 1) # [seq_len, *, attention_width]
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2) # [*, seq_len, attention_width]
+        if self.average_output:
+            x = x.mean(dim=1)
+            x = x @ self.proj
+        else:
+            x = x.reshape(x.shape[0], -1) # [*, attention_width * seq_len]
+            x = self.proj(x)
+        return x
+
+# class Voxel3dConvEncoder(nn.Module):
+#     def __init__(self, dims: List[int], layers: int, output_dim: int, act_layer: Callable = nn.GELU):
+#         super().__init__()
+#         channels = [1, 64, 64, 64, 64, 64, 32, 32, 32, 32, 32, 32, 32]
+#         strides = [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2]
+#         self.conv_blocks = nn.ModuleList([
+#             self._get_conv_layer(channels[i], channels[i + 1], kernel_size=3, stride=strides[i], act_layer=act_layer)
+#             for i in range(layers)
+#         ])
+#         for n in range(layers):
+#             stride = strides[n]
+#             dialation = 1
+#             padding = 0
+#             kernel = 3
+#             # (d + 2*padding - dialation*(kernel - 1) - 1)/(stride) + 1
+#             dims = [int((d + 2*padding - dialation*(kernel - 1) - 1)/(stride) + 1) for d in dims]
+#         logging.info(f"Expected output shape: {dims}")
+#         self.mlp = MLP(channels[layers] * dims[0] * dims[1] * dims[2], output_dim, hidden_dim=output_dim*4, num_layers=3)
+        
+#     def _get_conv_layer(self, c_in, c_out, kernel_size, stride, act_layer):
+#         return nn.Sequential(
+#             nn.Conv3d(c_in, c_out, kernel_size=kernel_size, stride=stride),
+#             nn.Dropout3d(0.1),
+#             act_layer(),
+#             # nn.MaxPool3d(kernel_size=2, stride=2)
+#         )
+
+#     def forward(self, x: torch.Tensor):
+#         for block in self.conv_blocks:
+#             x = block(x)
+#         # logging.info(f"Actual output shape: {x.shape}")
+#         x = x.view(x.shape[0], -1)
+#         return self.mlp(x)
+
 class Voxel3dConvEncoder(nn.Module):
     def __init__(self, dims: List[int], layers: int, output_dim: int, act_layer: Callable = nn.GELU):
         super().__init__()
@@ -616,12 +759,25 @@ class VoxelCLIP(nn.Module):
             assert voxel_cfg.config_3d_conv is not None, "config_3d_conv is required for voxel_type=3d"
             # Uses a 3D convolutional network to encode voxel data.
             # raise NotImplementedError("3D voxel encoder not implemented")
-            self.voxel_encoder = Voxel3dConvEncoder(
+            # self.voxel_encoder = Voxel3dConvEncoder(
+            #     dims=voxel_cfg.config_3d_conv["dims"],
+            #     layers=voxel_cfg.config_3d_conv["layers"],
+            #     output_dim=embed_dim,
+            #     act_layer=act_layer,
+            # )
+            self.voxel_encoder = NewVoxel3dConvEncoder(
                 dims=voxel_cfg.config_3d_conv["dims"],
-                layers=voxel_cfg.config_3d_conv["layers"],
+                attention_width=64,
                 output_dim=embed_dim,
-                act_layer=act_layer,
+                average_output=False,
+                act_layer=act_layer
             )
+            # self.voxel_encoder = ClassVoxel3dConvEncoder(
+            #     dims=voxel_cfg.config_3d_conv["dims"],
+            #     width = 512,
+            #     output_dim=embed_dim,
+            #     act_layer=act_layer
+            # )
         elif voxel_type == "3d-vision-transformer": 
             # Uses the visual transformer with a large channel dimension to encode voxel data.
             assert voxel_cfg.config_2d_visual_transformer is not None, "config_2d_visual_transformer is required for voxel_type=3d"
